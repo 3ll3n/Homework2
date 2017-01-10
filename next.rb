591f57811d55cacd59ef842c84a0c671056ca92c
3ll3n <ellengrafton01@gmail.com>
2017-01-10 16:18:55 +0000

    Added shortened format months and fairground admittance functions

--- next.rb
+++ next.rb
@@ -66,4 +66,54 @@ end
 
 puts month(1)
 
+def month(number)
+  case number
+    when 1
+      puts "Jan"
+    when 2
+      puts "Feb"
+    when 3
+      puts "Mar"
+    when 4
+      puts "Apr"
+    when 5
+      puts "May"
+    when 6
+      puts "June"
+    when 7
+      puts "July"
+    when 8
+      puts "Aug"
+    when 9
+      puts "Sept"
+    when 10
+      puts "Oct"
+    when 11
+      puts "Nov"
+    when 12
+      puts "Dec"
+  end
+end
+
+puts month(8)
+
+
+
+def fairground_admittance(weight, height)
+  if(weight >= 40 && height >= 1.4)
+    puts "enter"
+  else
+    puts "sorry"
+  end
+end
+
+puts "In order to establish if you can go on the fairground round please can you confirm the following: "
+puts "weight in kilos"
+weight = gets.chomp.to_i
+puts "height in meters"
+height = gets.chomp.to_f
+
+puts fairground_admittance(weight, height)
+
+
 
