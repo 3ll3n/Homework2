01f94f625e9ec4779f64fe0e6119f040c2db0487
3ll3n <ellengrafton01@gmail.com>
2017-01-10 15:11:26 +0000

    Added length_of_name funcitionality

--- next.rb
+++ next.rb
@@ -9,3 +9,11 @@ def multiply(number_1, number_2)
 end
 
 puts multiply(2,3)
+
+def length_of_name(firstname, secondname)
+  return firstname.length + secondname.length
+end
+
+puts length_of_name("John", "Smith")
+
+
